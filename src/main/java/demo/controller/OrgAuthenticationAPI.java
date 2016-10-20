3333c26160e17e33990b098f50d05548bf20ce7a
zhengyunfei <zhengyf@ctyun.cn>
2016-10-20 15:57:35 +0800

    个人用户认证和机构用户认证API文档

--- src/main/java/demo/controller/OrgAuthenticationAPI.java
+++ src/main/java/demo/controller/OrgAuthenticationAPI.java
@@ -22,12 +22,12 @@ public class OrgAuthenticationAPI {
     UserMapper userMapper;
 
     @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>机构用户认证第一步</font>)",position = 1,nickname = "机构用户认证第一步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>orgName</td><td>否</td><td>机构名称</td></tr>" +
             "<tr><td>orgDutypersonName</td><td>否</td><td>机构负责人名称</td></tr>" +
             "<tr><td>orgDutypersonPosition</td><td>否</td><td>机构负责人职位</td></tr>" +
             "<tr><td>orgDutypepsonEmaile</td><td>否</td><td>机构负责人邮箱</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 机构用户认证API接口 第一步
 	 */
@@ -39,9 +39,9 @@ public class OrgAuthenticationAPI {
     }
 
     @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>机构用户认证第二步</font>)",position = 2,nickname = "机构用户认证第二步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>orgAssets</td><td>否</td><td>机构净资产</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 机构用户认证API接口 第二步
 	 */
@@ -54,10 +54,10 @@ public class OrgAuthenticationAPI {
 
 
     @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>机构用户认证第三步</font>)",position = 3,nickname = "机构用户认证第三步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>ORG_PERSON_CARD_URL</td><td>否</td><td>机构负责人照片：需要将form-data; name=ORG_PERSON_CARD_URL</td></tr>" +
             "<tr><td>ORG_LICENSE_URL</td><td>否</td><td>机构营业执照：需要将form-data; name=ORG_LICENSE_URL</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 机构用户认证API接口 第三步
 	 */
@@ -67,4 +67,26 @@ public class OrgAuthenticationAPI {
         String myJsonData="";
         return myJsonData;
     }
+
+
+    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>批量更新机构用户信息</font>)",position = 3,nickname = "批量更新机构用户信息", notes = "<h2>传递参数</h2>:<br>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
+            "<tr><td>orgName</td><td>否</td><td>机构名称</td></tr>" +
+            "<tr><td>orgDutypersonName</td><td>否</td><td>机构负责人名称</td></tr>" +
+            "<tr><td>orgDutypersonPosition</td><td>否</td><td>机构负责人职位</td></tr>" +
+            "<tr><td>orgDutypepsonEmaile</td><td>否</td><td>机构负责人邮箱</td></tr>" +
+            "<tr><td>orgAssets</td><td>否</td><td>机构净资产</td></tr>" +
+            "<tr><td>ORG_PERSON_CARD_URL</td><td>否</td><td>机构负责人照片：需要将form-data; name=ORG_PERSON_CARD_URL</td></tr>" +
+            "<tr><td>ORG_LICENSE_URL</td><td>否</td><td>机构营业执照：需要将form-data; name=ORG_LICENSE_URL</td></tr>" +
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
+    /*
+	 * 批量更新机构用户信息
+	 */
+    @RequestMapping(value = "/api/orz/step4", method = RequestMethod.POST)
+    @ResponseBody
+    public Object step4(@RequestParam String orgName, @RequestParam String orgDutypersonName, @RequestParam String orgDutypersonPosition,@RequestParam String orgDutypepsonEmaile,@RequestParam String orgAssets,@RequestParam String ORG_PERSON_CARD_URL, @RequestParam String ORG_LICENSE_URL,@RequestParam String token, HttpServletRequest request) {
+        String myJsonData="";
+        return myJsonData;
+    }
+
 }
--- src/main/java/demo/controller/UserAuthenticationAPI.java
+++ src/main/java/demo/controller/UserAuthenticationAPI.java
@@ -21,12 +21,12 @@ public class UserAuthenticationAPI {
     @Autowired
     UserMapper userMapper;
 
-    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第一步</font>)",position = 1,nickname = "用户认证第一步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第一步</font>)",position = 1,nickname = "用户认证第一步", notes = "<font color='green'>传递参数</font>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>name</td><td>否</td><td>用户名</td></tr>" +
             "<tr><td>email</td><td>否</td><td>邮箱</td></tr>" +
             "<tr><td>idCardNo</td><td>否</td><td>身份证号码</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 用户认证API接口 第一步
 	 */
@@ -37,13 +37,13 @@ public class UserAuthenticationAPI {
         return myJsonData;
     }
 
-    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第二步</font>)",position = 2,nickname = "用户认证第二步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第二步</font>)",position = 2,nickname = "用户认证第二步", notes = "<font color='green'>传递参数</font>:<br>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>company</td><td>否</td><td>公司名称</td></tr>" +
             "<tr><td>position</td><td>否</td><td>职位</td></tr>" +
             "<tr><td>assest</td><td>否</td><td>金融资产量</td></tr>" +
             "<tr><td>arpm</td><td>否</td><td>近三年平均收入</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 用户认证API接口 第二步
 	 */
@@ -55,17 +55,56 @@ public class UserAuthenticationAPI {
     }
 
 
-    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第三步</font>)",position = 3,nickname = "用户认证第三步", notes = "<h2>传递参数</h2>:<br>" +
-            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>备注</th></tr>" +
+    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>用户认证第三步</font>)",position = 3,nickname = "用户认证第三步", notes = "<font color='green'>传递参数</font>:<br>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
             "<tr><td>USERCARD_IMAGE</td><td>否</td><td>名片：需要将form-data; name=USERCARD_IMAGE</td></tr>" +
             "<tr><td>IDCARD_IMAGE</td><td>否</td><td>身份证照片：需要将form-data; name=IDCARD_IMAGE</td></tr>" +
-            "<tr><td>token</td><td>否</td><td>token</td></tr>" )
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
     /*
 	 * 用户认证API接口 第三步
 	 */
     @RequestMapping(value = "/api/urz/step3", method = RequestMethod.POST)
     @ResponseBody
     public Object step3(@RequestParam String USERCARD_IMAGE, @RequestParam String IDCARD_IMAGE,@RequestParam String token, HttpServletRequest request) {
+        String myJsonData="";
+        return myJsonData;
+    }
+
+    /**
+     * 批量更新用户信息接口
+     * @param name
+     * @param sex
+     * @param email
+     * @param idCardNo
+     * @param company
+     * @param position
+     * @param assest
+     * @param arpm
+     * @param USERCARD_IMAGE
+     * @param IDCARD_IMAGE
+     * @param token
+     * @param request
+     * @return
+     */
+    @ApiOperation(httpMethod = "POST", value = "(<font color='blue'>批量更新用户信息接口</font>)",position = 4,nickname = "批量更新用户信息接口", notes = "<font color='green'>传递参数</font>:<br>" +
+            "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
+            "<tr><td>company</td><td>否</td><td>公司名称</td></tr>" +
+            "<tr><td>position</td><td>否</td><td>职位</td></tr>" +
+            "<tr><td>assest</td><td>否</td><td>金融资产量</td></tr>" +
+            "<tr><td>arpm</td><td>否</td><td>近三年平均收入</td></tr>" +
+            "<tr><td>company</td><td>否</td><td>公司名称</td></tr>" +
+            "<tr><td>position</td><td>否</td><td>职位</td></tr>" +
+            "<tr><td>assest</td><td>否</td><td>金融资产量</td></tr>" +
+            "<tr><td>arpm</td><td>否</td><td>近三年平均收入</td></tr>" +
+            "<tr><td>USERCARD_IMAGE</td><td>否</td><td>名片：需要将form-data; name=USERCARD_IMAGE</td></tr>" +
+            "<tr><td>IDCARD_IMAGE</td><td>否</td><td>身份证照片：需要将form-data; name=IDCARD_IMAGE</td></tr>" +
+            "<tr><td>token</td><td>否</td><td>token</td></tr></table>" )
+    /*
+	 * 用户认证API接口 第三步
+	 */
+    @RequestMapping(value = "/api/urz/step4", method = RequestMethod.POST)
+    @ResponseBody
+    public Object step4(@RequestParam String name, @RequestParam String sex, @RequestParam String email,@RequestParam String idCardNo,@RequestParam String company, @RequestParam String position, @RequestParam String assest,@RequestParam String arpm,@RequestParam String USERCARD_IMAGE, @RequestParam String IDCARD_IMAGE,@RequestParam String token, HttpServletRequest request) {
         String myJsonData="";
         return myJsonData;
     }
