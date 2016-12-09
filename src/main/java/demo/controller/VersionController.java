379a41dc3aa783a9fd56f85a52293156d1307d05
zhengyunfei <zhengyf@ctyun.cn>
2016-12-09 19:12:42 +0800

    app2.0接口文档

--- src/main/java/demo/controller/VersionController.java
+++ src/main/java/demo/controller/VersionController.java
@@ -30,25 +30,30 @@ public class VersionController {
             "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
             "{\"code\":1005,\"message\":\"token无效\",\"data\":null}" +
             "</code></pre></div>"+
+            "<h4>没有站内信时返回</h4>" +
+            "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
+            "{\n" +
+            "    \"data\": {\n" +
+            "        \"list\": [ ]\n" +
+            "    }, \n" +
+            "    \"code\": 0, \n" +
+            "    \"message\": \"成功\"\n" +
+            "}" +
+            "</code></pre></div>"+
             "<h4>返回结果集</h4>" +
             "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
-            "{" +
+            "{\n" +
             "    \"data\": {\n" +
             "        \"list\": [\n" +
             "            {\n" +
-            "                \"title\": \"审核通过\", \n" +
-            "                \"content\": \"测试用户消息\", \n" +
-            "                \"sendTime\": \"2015-07-22 15:34:23\"\n" +
+            "                \"title\": \"3条短信又没有\", \n" +
+            "                \"content\": \"有啊\", \n" +
+            "                \"sendTime\": \"1418277173\"\n" +
             "            }, \n" +
             "            {\n" +
             "                \"title\": \"3条短信又没有\", \n" +
             "                \"content\": \"有啊\", \n" +
-            "                \"sendTime\": \"2014-12-11 13:52:53\"\n" +
-            "            }, \n" +
-            "            {\n" +
-            "                \"title\": \"3条短信又没有\", \n" +
-            "                \"content\": \"有啊\", \n" +
-            "                \"sendTime\": \"2014-12-11 13:52:53\"\n" +
+            "                \"sendTime\": \"1418275553\"\n" +
             "            }\n" +
             "        ]\n" +
             "    }, \n" +
@@ -64,6 +69,46 @@ public class VersionController {
         String myJsonData="";
         return myJsonData;
     }
+/**********************判断是否有最新的消息*********************************************************************/
+@ApiOperation(httpMethod = "POST", value = "6:是否有最新站内信消息",position = 1,nickname = "是否有最新站内信消息", notes = "" +
+        "<table border=\"1\"><tr><th>字段</th><th>是否为空</th><th>\t备注</th></tr>" +
+        "<tr><td>token</td><td>否</td><td>token</td></tr>" +
+        "<tr><td>lastTime</td><td>否</td><td>最后获取红包消息的时间</td></tr></table>" +
+        "<h4>返回结果</h4>" +
+        "<h4>token无效</h4>" +
+        "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
+        "{\"code\":1005,\"message\":\"token无效\",\"data\":null}" +
+        "</code></pre></div>"+
+        "<h4>没有最新站内信消息</h4>" +
+        "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
+        "{\n" +
+        "    \"code\": 0, \n" +
+        "    \"message\": \"成功\", \n" +
+        "    \"data\": {\n" +
+        "        \"code\": 0\n" +
+        "    }\n" +
+        "}" +
+        "</code></pre></div>"+
+        "<h4>有最新站内信消息:isExsitNewMessage:0没有1有 myCouponCount：我的优惠券个数</h4>" +
+        "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
+        "{\n" +
+        "    \"code\": 0, \n" +
+        "    \"message\": \"成功\", \n" +
+        "    \"data\": {\n" +
+        "        \"isExsitNewMessage\": 1, \n" +
+        "        \"myCouponCount\": 4\n" +
+        "    }\n" +
+        "}" +
+
+        "</code></pre></div>"
+)
+@RequestMapping(value = "/api/newstation/isExsitNew", method = RequestMethod.POST)
+@ResponseBody
+public Object isExsitNews(@RequestParam String token,@RequestParam String lastTime){
+    String myJsonData="";
+    return myJsonData;
+}
+/**********************判断是否有最新的消息*********************************************************************/
 
 
     @ApiOperation(httpMethod = "POST", value = "2:私人订制内容新增",position = 2,nickname = "私人订制内容新增", notes = "" +
@@ -162,7 +207,7 @@ public class VersionController {
             "}" +
             "</code></pre></div>"+
 
-            "<h4>返回结果集:status:0未使用1已使用</h4>" +
+            "<h4>返回结果集</h4>" +
             "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
             "{\n" +
             "    \"code\": 0, \n" +
@@ -184,6 +229,7 @@ public class VersionController {
     }
 
 
+    /***********************************************我的优惠券*****************************************************************/
     @ApiOperation(httpMethod = "POST", value = "5:我的优惠券",position = 4,nickname = "我的优惠券", notes = "" +
             "<h4>没有获取任何优惠券时返回</h4>" +
 
@@ -195,33 +241,46 @@ public class VersionController {
             "}" +
             "</code></pre></div>"+
 
-            "<h4>返回结果集</h4>" +
+            "<h4>返回结果集{status:0未使用1已使用}</h4>" +
             "<div class=\"block response_body json\"><pre class=\"json\"><code>"+
             "{\n" +
-            "    \"code\": 0, \n" +
-            "    \"message\": \"成功\", \n" +
             "    \"data\": {\n" +
             "        \"list\": [\n" +
             "            {\n" +
-            "                \"name\": \"测试红包11\", \n" +
-            "                \"status\": 0, \n" +
-            "                \"money\": 20000\n" +
+            "                \"title\": \"投资支持资金2\", \n" +
+            "                \"money\": 26000, \n" +
+            "                \"status\": 1\n" +
             "            }, \n" +
             "            {\n" +
-            "                \"name\": \"APP专享新人注册大礼\", \n" +
-            "                \"status\": 0, \n" +
-            "                \"money\": 10000\n" +
+            "                \"title\": \"投资支持资金\", \n" +
+            "                \"money\": 1000, \n" +
+            "                \"status\": 1\n" +
+            "            }, \n" +
+            "            {\n" +
+            "                \"title\": \"投资支持资金\", \n" +
+            "                \"money\": 1000, \n" +
+            "                \"status\": 0\n" +
+            "            }, \n" +
+            "            {\n" +
+            "                \"title\": \"投资支持资金\", \n" +
+            "                \"money\": 26000, \n" +
+            "                \"status\": 0\n" +
             "            }\n" +
             "        ]\n" +
-            "    }\n" +
+            "    }, \n" +
+            "    \"code\": 0, \n" +
+            "    \"message\": \"成功\"\n" +
             "}" +
 
             "</code></pre></div>"
     )
-    @RequestMapping(value = "/api/hd/list", method = RequestMethod.POST)
+    @RequestMapping(value = "/api/userCoupon/list", method = RequestMethod.POST)
     @ResponseBody
-    public Object hdlist(@RequestParam String token){
+    public Object hdlist(@RequestParam String token,@RequestParam String pageNow,@RequestParam String pageSize){
         String myJsonData="";
         return myJsonData;
     }
+
+
+    /***********************************************我的优惠券*****************************************************************/
 }
